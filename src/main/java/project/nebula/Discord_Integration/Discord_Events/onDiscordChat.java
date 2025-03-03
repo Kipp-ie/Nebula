ad353df0d640952e18b7ee604f63bcaff383eb7f
Jack <jacktcoon@gmail.com>
2025-03-03 14:17:34 -0800

    Added MessageBuilder.java
    and Handled the Attachments

--- src/main/java/project/nebula/Discord_Integration/Discord_Events/onDiscordChat.java
+++ src/main/java/project/nebula/Discord_Integration/Discord_Events/onDiscordChat.java
@@ -3,10 +3,12 @@ package project.nebula.Discord_Integration.Discord_Events;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
+import project.nebula.messages.MessageBuilder;
 
 public class onDiscordChat extends ListenerAdapter {
     private final FileConfiguration config;
@@ -16,15 +18,36 @@ public class onDiscordChat extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) {//If a message is posted from a bot or system, ignore.
-            return; //Stop the code.
-        } else {//If author is not bot or system then:
-            if (!event.getAuthor().isSystem()) {
-                if (event.getChannel().equals(event.getJDA().getTextChannelById(config.getString("Discord_ChatID")))) {
-                    var mm = MiniMessage.miniMessage();
-                    Component parsed = mm.deserialize("<bold><blue>Discord</bold></blue> - " + event.getAuthor().getName() + " | " + event.getMessage().getContentDisplay());
-                    Bukkit.broadcast(parsed);
-            }
+            if (event.getAuthor().isBot() || event.getAuthor().isSystem()) {//If a message is posted from a bot or system, ignore.
+                return; //Stop the code.
+            } else {//If author is not bot or system then:
+                if (!event.getAuthor().isSystem()) {
+                    if (event.getChannel().equals(event.getJDA().getTextChannelById(config.getString("Discord_ChatID")))) {
+                        var mm = MiniMessage.miniMessage();
+                        Component parsed = mm.deserialize("<bold><blue>Discord</bold></blue> - " + event.getAuthor().getName() + " | " + event.getMessage().getContentDisplay());
+                        Bukkit.broadcast(parsed);
+                        if(!event.getMessage().getAttachments().isEmpty()) {
+                            handleAttachment(event);
+                        }else {
+                            handleMessage(event);
+                        }
+                    }
+                }
             }
         }
     }
+
+    private void handleMessage(MessageReceivedEvent event) {
+        Component message = new MessageBuilder().text("&9&lDiscord &7- &f" + event.getAuthor().getName() + " &7| &f" + event.getMessage().getContentDisplay()).build();
+        Bukkit.broadcast(message);
+    }
+
+    private void handleAttachment(MessageReceivedEvent event) {
+        MessageBuilder message = new MessageBuilder().text("&9&lDiscord &7- &f" + event.getAuthor().getName() + " &7| &f" + event.getMessage().getContentDisplay());
+        event.getMessage().getAttachments().forEach(attachment -> {
+            message.text("&f&n&l[ATTACHMENT] ").withCopyToClipboard(attachment.getUrl());
+        });
+    }
+
 }
+
